347ff7716862ef81c996e3d55ef3d1df771a3c86
yusunjo <tnswh2023@gmail.com>
2021-01-26 22:53:30 +0900

    게시글에 하트 추가

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,19 +1,49 @@
 import { Card } from 'antd';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { HeartTwoTone, HeartOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
 
 const PostCard = ({ post }) => {
-    return (
-        <div>
-            <Card type="inner" title={post.title} extra={<a href="#">More</a>}>
-                {post.title}
-            </Card>
-        </div>
-    );
+  const [liked, setLiked] = useState(false);
+  const id = useSelector((state) => state.user.me?.id);
+  const onLike = useCallback(() => {
+    console.log('like');
+  }, []);
+
+  const onUnLike = useCallback(() => {
+    console.log('unlike');
+  }, []);
+
+  //   const liked = post.Likers.find((v) => v.id == id);
+  return (
+    <div>
+      <Card
+        type="inner"
+        title={post.title}
+        extra={[
+          <div>
+            <a href="#">More</a>
+          </div>,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#ebf96"
+              key="heart"
+              onClick={onUnLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onLike} />
+          ),
+        ]}
+      >
+        {post.title}
+      </Card>
+    </div>
+  );
 };
 
 PostCard.propTypes = {
-    post: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
 };
 
 export default PostCard;
